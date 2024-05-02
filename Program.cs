db27621f15494053f72f683ea6bf2537faf8d7e9
manumr13 <manumr8296@gmail.com>
2024-05-02 16:15:49 +0530

    adding

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//simple calculater
+
+using System;
+
+namespace calculater{
+    class Calculater{
+        static void Main(string[] args){
+
+            Console.WriteLine("Enter the 1st and 2nd number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operation you want to do +, -, /, *");
+            string symbol = Console.ReadLine();
+            switch(symbol){
+                case "+":
+                Console.WriteLine(num1+num2);
+                break;
+                case "-":
+                Console.WriteLine(num1-num2);
+                break;
+                case "/":
+                Console.WriteLine(num1/num2);
+                break;
+                case "*":
+                Console.WriteLine(num1*num2);
+                break;
+                default:
+                Console.WriteLine("enter the correct operation");
+                break;
+            }
+            }
+    }
+}
